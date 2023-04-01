2b1643c2ed6da2f78c580ac8e926342edf738e9e
ppariah <roniem2008@hotmail.com>
2023-04-01 21:05:52 +0200

    Cleaned up event page.. again

--- src/main/java/com/group/p2_socializer/EventPage/EventPageController.java
+++ src/main/java/com/group/p2_socializer/EventPage/EventPageController.java
@@ -6,9 +6,11 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -18,8 +20,22 @@ import java.io.IOException;
 public class EventPageController {
     @FXML
     public AnchorPane centerPane;
-    private Label eventTitle;
-    private Label eventDate;
+    @FXML
+    public ScrollPane scrollPane;
+    @FXML
+    public VBox eventInfoVBox;
+    @FXML
+    public Label eventDateLabel;
+    @FXML
+    public Label eventTitleLabel;
+    @FXML
+    public Label eventLocationLabel;
+    @FXML HBox organiserHBox;
+    @FXML
+    public Label byLabel;
+    @FXML
+    public Label eventOrganiserLabel;
+
 
     public void loadEventPage(MouseEvent event, String eventName, String eventDate, String eventOrganiser, String eventDescription, String eventCity, String eventCountry) {
         try {
@@ -30,42 +46,46 @@ public class EventPageController {
             FXMLLoader loader = new FXMLLoader(EventPageController.class.getResource("/com/group/p2_socializer/event_page.fxml"));
             Parent root = loader.load();
 
-            Label eventDateLabel = new Label(eventDate);
-            eventDateLabel.setStyle("-fx-font-family: 'Arial'; -fx-font-weight: bold; -fx-font-size: 13; -fx-text-fill: #797878;");
-            eventDateLabel.setLayoutX(30);
-            eventDateLabel.setLayoutY(210);
+            // Get reference to centerPane FXML file
+            ScrollPane scrollPane = (ScrollPane) root.lookup("#scrollPane");
+            AnchorPane centerPane = (AnchorPane) scrollPane.getContent();
+            VBox eventInfoVBox = (VBox) centerPane.lookup("#eventInfoVBox");
+            HBox organiserHBox = (HBox) eventInfoVBox.lookup("#organiserHBox");
 
-            Label eventTitleLabel = new Label(eventName + ": ");
-            eventTitleLabel.setLayoutX(30.0);
-            eventTitleLabel.setLayoutY(225);
+            //Label eventDateLabel = new Label(eventDate);
+            Label eventDateLabel = (Label) eventInfoVBox.lookup("#eventDateLabel");
+            eventDateLabel.setText(eventDate);
+            eventDateLabel.setStyle("-fx-font-family: 'Arial'; -fx-font-weight: bold; -fx-font-size: 13; -fx-text-fill: #797878;");
+            //eventDateLabel.setLayoutX(30);
+            //eventDateLabel.setLayoutY(210);
+
+            Label eventTitleLabel = (Label) eventInfoVBox.lookup("#eventTitleLabel" );
+            eventTitleLabel.setText(eventName);
             eventTitleLabel.setFont(Font.font("Eras Bold ITC", 30));
 
-            Label eventLocation = new Label( eventCity + ", " + eventCountry);
-            eventLocation.setLayoutX(30);
-            eventLocation.setLayoutY(260);
+            Label eventLocation =  (Label) eventInfoVBox.lookup("#eventLocationLabel");
+            eventLocation.setText(eventCity + ", " + eventCountry);
             eventLocation.setStyle("-fx-font-family: 'Arial'; -fx-font-weight: bold; -fx-font-size: 11; -fx-text-fill: #797878;");
 
-
-            Label eventOrganiserLabel = new Label("by " + eventOrganiser);
-            eventOrganiserLabel.setLayoutX(30);
-            eventOrganiserLabel.setLayoutY(275);
-            eventOrganiserLabel.setStyle("-fx-font-family: 'Eras ITC'; -fx-font-style: italic; -fx-font-weight: bold; -fx-font-size: 15; -fx-text-fill: #67f3ff;");
+            Label byLabel = (Label) organiserHBox.lookup("#byLabel");
+            byLabel.setText("by ");
+            byLabel.setStyle("-fx-font-family: 'Arial'; -fx-font-weight: bold; -fx-font-size: 11; -fx-text-fill: #797878;");
 
 
+            Label eventOrganiserLabel = (Label) organiserHBox.lookup("#eventOrganiserLabel");
+            eventOrganiserLabel.setText(eventOrganiser);
+            eventOrganiserLabel.setStyle("-fx-font-family: 'Arial'; -fx-font-style: italic; -fx-font-weight: bold; -fx-font-size: 12; -fx-text-fill: #000000;");
 
             Label eventDescriptionLabel = new Label(eventDescription);
-            eventDescriptionLabel.setLayoutX(32);
-            eventDescriptionLabel.setLayoutY(295);
+            eventDescriptionLabel.setLayoutX(36);
+            eventDescriptionLabel.setLayoutY(450);
             eventDescriptionLabel.setFont(Font.font("Arial", 13));
 
-            // Get reference to centerPane FXML file
-            AnchorPane centerPane = (AnchorPane) root.lookup("#centerPane");
+
+
 
             // Add the label to the center pane
-            centerPane.getChildren().add(eventDateLabel);
-            centerPane.getChildren().add(eventTitleLabel);
-            centerPane.getChildren().add(eventOrganiserLabel);
-            centerPane.getChildren().add(eventLocation);
+            centerPane.getChildren().add(eventInfoVBox);
             centerPane.getChildren().add(eventDescriptionLabel);
 
 
