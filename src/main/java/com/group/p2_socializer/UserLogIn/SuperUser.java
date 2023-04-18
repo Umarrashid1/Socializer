b27dcd62535427afd86219f678526410e266b1af
Draellemeistro <117720444+Draellemeistro@users.noreply.github.com>
2023-04-18 02:56:28 +0200

    WIP: profile & spotty profile functionality

---
+++ src/main/java/com/group/p2_socializer/UserLogIn/SuperUser.java
@@ -0,0 +1,8 @@
+package com.group.p2_socializer.UserLogIn;
+
+public class SuperUser extends User {
+    /*
+    Not certain if inheritance is the most elegant approach at distinguishing user types.
+    It does seem somewhat useful to establish the hierachy of user types.
+     */
+}
